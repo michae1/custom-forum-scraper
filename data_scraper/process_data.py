0fe39c9e8be7fad4a25c43ad74af8844287433a3
Michael Plakhov <qotsaman@gmail.com>
2015-01-03 01:11:11 +0200

    process script cleanup

--- data_scraper/process_data.py
+++ data_scraper/process_data.py
@@ -4,11 +4,13 @@ import binascii
 import re
 from models import Post, get_session
 import operator
-
-print "started"
-
 import sys
 import codecs
+
+'''
+Sample data processing script (this one to calculate words freq)
+'''
+
 sys.stdout = codecs.getwriter('utf8')(sys.stdout)
 sys.stderr = codecs.getwriter('utf8')(sys.stderr)
 
@@ -19,7 +21,6 @@ def update_words_counts(txt):
 	global tw
 	global word_counts
 	utxt = txt.encode('utf8')
-	#print utxt
 	words = re.findall(u"[\w']+", txt, re.UNICODE)
 	# remove short (1-2)
 	for w in words:
@@ -27,7 +28,6 @@ def update_words_counts(txt):
 		if len(word)<4:
 			continue
 		tw += 1	
-		# print word_counts
 		if word not in word_counts:	
 			word_counts[word] = 1
 		else:
@@ -36,18 +36,11 @@ def update_words_counts(txt):
 session = get_session()
 
 posts = session.query(Post).all()
-
-#1 get total counts
-
 for post in posts:
 	update_words_counts(post.text)
-
-# for wc in word_counts:
-# 	print "%s: %s"%(wc, word_counts[wc])
 
 swc = sorted(word_counts.items(), key=operator.itemgetter(1))
 for w in swc:
 	print "%s: %s"%(w[0], w[1])
-# print "Total: %s"%tw
 
 author_counts = {}
